e071274dfafcfeadb8f35825c9379d55deac5910
Senye <569742406@qq.com>
2021-02-25 21:19:22 +0800

    2020.2.25

---
+++ src/main/java/com/ppdz/side/SideApplication.java
@@ -0,0 +1,11 @@
+package com.ppdz.side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SideApplication.class, args);
+    }
+}
